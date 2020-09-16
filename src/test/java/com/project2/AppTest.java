c88a6ad78e60d59691a2698970cf106b16aa09dc
matthew becker <mattbecker7@gmail.com>
2020-09-16 17:10:11 -0400

    updated name

--- src/test/java/com/project2/AppTest.java
+++ src/test/java/com/project2/AppTest.java
@@ -36,8 +36,8 @@ public class AppTest
         assertTrue( true );
     }
 
-    public void testApp2()
-    {
-        assertTrue( false );
-    }
+    // public void testApp2()
+    // {
+    //     assertTrue( false );
+    // }
 }
